6dca77ca3388496988a8f9d192407ae8ed7f09a1
kwak123 <kwak123@gmail.com>
2019-11-19 19:03:06 -0500

    email options

--- src/mailgun/index.ts
+++ src/mailgun/index.ts
@@ -7,7 +7,24 @@ const VERSION = "v3"
 
 const API_URL = [BASE_URL, VERSION, TEST_EMAIL_DOMAIN].join("/")
 
+export interface EmailOptions {
+  // Who to send it to
+  to: string
+  // Email subject
+  subject: string
+  // Email body
+  body: string
+}
+
+const MailgunConstants = {
+  TO: "to",
+  FROM: "from",
+  SUBJECT: "subject",
+  TEXT: "text",
+}
+
 class Mailgun {
+  private from = "Samlysia <samlysia@kwakanalia.com>"
   private apiKey: string
   apiClient: AxiosInstance
 
@@ -23,13 +40,13 @@ class Mailgun {
   }
 
   // Test
-  sendEmail() {
+  sendEmail(emailOptions: EmailOptions) {
     const mailGunSendEmailEndpoint = `${API_URL}/messages`
     const formData = new FormData()
-    formData.append("to", "elysia.hwang@gmail.com")
-    formData.append("from", "The BB! <thebb@kwakanalia.com>")
-    formData.append("subject", "Does it work?")
-    formData.append("text", "If this reaches you, look at me in an annoyed way")
+    formData.append(MailgunConstants.TO, emailOptions.to)
+    formData.append(MailgunConstants.FROM, this.from)
+    formData.append(MailgunConstants.SUBJECT, emailOptions.subject)
+    formData.append(MailgunConstants.TEXT, emailOptions.body)
 
     return this.apiClient.post(mailGunSendEmailEndpoint, formData, {
       headers: formData.getHeaders(),
