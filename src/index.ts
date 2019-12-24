9a15e757cd00514c777383f1ed50f8396ffb8414
kwak123 <kwak123@gmail.com>
2019-12-24 11:20:29 -0500

    add request logger

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
-import express from "express"
+import express, { Request, Response, NextFunction } from "express"
 import path from "path"
 import "./redis/client"
 import "./mongo/client"
 
 import configureRouter from "./routes"
 
+const logRequest = (req: Request, res: Response, next: NextFunction) => {
+  console.log(`Received ${req.method}: ${req.url}`)
+  next()
+}
+
 const app = express()
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, "../dist")))
+
+app.use(logRequest)
 
 configureRouter(app)
 
