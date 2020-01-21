439d5be6be69e9ecf1a4ca348941d3d49a113938
kwak123 <kwak123@gmail.com>
2020-01-20 21:59:34 -0500

    strip out unused mailgun stuff

--- src/mailgun/rsvpEmailBody.ts
+++ src/mailgun/rsvpEmailBody.ts
@@ -3,3 +3,29 @@ export default `
   <p>This is the RSVP template</p>
 </div>
 `
+
+// import hashingUtils from "../utils/hashing"
+
+// const makeEmailBody = (email: string) => {
+//   let BASE_URL = `http://localhost:3000/`
+//   if (process.env.NODE_ENV && process.env.NODE_ENV === "prod") {
+//     BASE_URL = `https://kwakanalia.wedding/`
+//   }
+//   const CONFIRM_PAGE = `confirm/`
+
+//   const hashedEmail = hashingUtils.makeEmailHash(email)
+//   const ENDPOINT = `?email=${hashedEmail}`
+
+//   const linkWithHash = `${BASE_URL}${CONFIRM_PAGE}${ENDPOINT}`
+
+//   return `
+//     <div>
+//       <p>This is the RSVP template</p>
+//       <a href=${linkWithHash}>Confirm Here</a>
+//     </div>
+//   `
+// }
+
+// export default {
+//   makeEmailBody,
+// }
