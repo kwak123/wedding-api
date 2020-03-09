e6551bad1d5be665988ace74f4a8c6fa47199c31
kwak123 <kwak123@gmail.com>
2020-03-08 21:46:58 -0400

    use env for redis url for e2e

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -1,6 +1,8 @@
 import DbHelper from "./helpers/db"
+import request from "supertest"
+import app from "../src/app"
 
-describe("it", () => {
+describe("app", () => {
   let dbHelper: DbHelper
 
   beforeAll(async () => {
@@ -14,7 +16,42 @@ describe("it", () => {
     return
   })
 
-  it("should be true", () => {
-    expect(true).toBeTruthy
+  describe("guest", () => {
+    describe("add", () => {
+      it("should successfully add a guest", async () => {
+        const mockGuest = makeDummyGuest({
+          name: "sam",
+          email: "test@test.com ",
+        })
+        const res = await request(app)
+          .post(makeGuestUrl("add"))
+          .send(mockGuest)
+
+        expect(res.status).toEqual(200)
+        expect(res.body.name).toEqual("sam")
+      })
+    })
   })
+
+  const makeGuestUrl = (endpoint: string) => `/api/guest/${endpoint}`
+
+  interface Guest {
+    name: string
+    email: string
+    isPlusOneEligible: boolean
+    plusOneId: string | null
+    confirmed: boolean
+    attending: false
+    dietaryRestrictions: string[]
+  }
+  const makeDummyGuest = ({ name, email }: { name: string; email: string }) =>
+    ({
+      name,
+      email,
+      isPlusOneEligible: true,
+      plusOneId: null,
+      confirmed: false,
+      attending: false,
+      dietaryRestrictions: [],
+    } as Guest)
 })
