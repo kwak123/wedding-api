cdf12286896ce25c6d878be7c888767a895ac265
kwak123 <kwak123@gmail.com>
2020-03-08 20:43:28 -0400

    make dbHelper for clearing e2e db

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -1,4 +1,19 @@
+import DbHelper from "./helpers/db"
+
 describe("it", () => {
+  let dbHelper: DbHelper
+
+  beforeAll(async () => {
+    dbHelper = new DbHelper()
+    await dbHelper.resetDb()
+    return
+  })
+
+  afterAll(async () => {
+    await dbHelper.closeConnection()
+    return
+  })
+
   it("should be true", () => {
     expect(true).toBeTruthy
   })
--- e2e/helpers/db.ts
+++ e2e/helpers/db.ts
@@ -1,1 +1,19 @@
-import mongoose from "mongoose"
+import client, { Guest } from "../../src/mongo/client"
+
+const TEST_DB_NAME = "test"
+
+class DbTestHelper {
+  async resetDb() {
+    if (process.env.NODE_ENV !== "test") {
+      throw "You're trying to delete a non-test database!"
+    }
+    await Guest.deleteMany({})
+    return true
+  }
+
+  async closeConnection() {
+    return client.close()
+  }
+}
+
+export default DbTestHelper
