e12c72650518b783e1933acbb736030ebe0246cb
kwak123 <kwak123@gmail.com>
2020-03-17 00:03:55 -0400

    add some tests around the simple redis client

--- src/redis/redis.spec.ts
+++ src/redis/redis.spec.ts
@@ -8,22 +8,37 @@ describe("redis tests", () => {
     const result = await redisClient.cacheStringForRetrieval(mockKey, mockValue)
 
     expect(result).toBe(true)
-    await checkKeyExists(mockKey, mockValue)
+    await checkKeyValue(mockKey, mockValue)
   })
 
-  it("getCachedString pulls string from map and deletes string", () => {
+  it("getCachedString pulls string from map and deletes string", async () => {
     const mockKey = "key"
     const mockValue = "value"
+
+    await addKeyValuePair(mockKey, mockValue)
+    const result = await redisClient.getCachedString(mockKey)
+    expect(result).toEqual(mockValue)
+    await checkKeyValue(mockKey, null)
   })
 
-  const checkKeyExists = (key: string, expected: string) =>
+  const checkKeyValue = (key: string, expected: string) =>
     new Promise((resolve, reject) =>
       client.get(key, (err, res) => {
         if (err) {
           return reject(err)
         }
         expect(res).toEqual(expected)
-        resolve()
+        return resolve()
+      })
+    )
+
+  const addKeyValuePair = (key: string, value: string) =>
+    new Promise((resolve, reject) =>
+      client.set(key, value, (err, res) => {
+        if (err) {
+          return reject(err)
+        }
+        return resolve(res)
       })
     )
 })
