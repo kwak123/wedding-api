2285f1bcab696755d4b62463017167913599023a
kwak123 <kwak123@gmail.com>
2020-03-17 00:01:22 -0400

    move keyExists test out

--- src/redis/redis.spec.ts
+++ src/redis/redis.spec.ts
@@ -1,7 +1,29 @@
 import client from "./client"
+import redisClient from "./index"
 
 describe("redis tests", () => {
-  it("true", () => {
-    expect(true).toBe(true)
+  it("cacheStringForRetrieval stores string into map", async () => {
+    const mockKey = "key"
+    const mockValue = "value"
+    const result = await redisClient.cacheStringForRetrieval(mockKey, mockValue)
+
+    expect(result).toBe(true)
+    await checkKeyExists(mockKey, mockValue)
   })
+
+  it("getCachedString pulls string from map and deletes string", () => {
+    const mockKey = "key"
+    const mockValue = "value"
+  })
+
+  const checkKeyExists = (key: string, expected: string) =>
+    new Promise((resolve, reject) =>
+      client.get(key, (err, res) => {
+        if (err) {
+          return reject(err)
+        }
+        expect(res).toEqual(expected)
+        resolve()
+      })
+    )
 })
