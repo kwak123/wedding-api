37fe934add9e83aef0d79495190e7d2569381972
kwak123 <kwak123@gmail.com>
2020-03-18 15:08:51 -0400

    fix ts with cypress

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  preset: "ts-jest",
   testEnvironment: "node",
   setupFiles: ["dotenv/config"],
   setupFilesAfterEnv: ["./e2e/testSetup.ts"],
   collectCoverageFrom: ["**/*.ts", "!**/e2e/helpers/**", "!**/node_modules/**"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
 }
