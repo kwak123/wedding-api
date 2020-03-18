bffccdb3e081d2bf38ea5da4ac61c259a8901559
kwak123 <kwak123@gmail.com>
2020-03-18 19:42:46 -0400

    make dbhelper a dynamic import

--- e2e/testSetup.ts
+++ e2e/testSetup.ts
@@ -1,8 +1,10 @@
-import DbHelper from "./helpers/db"
 import "@testing-library/jest-dom"
 
-afterAll(async () => {
-  const dbHelper = new DbHelper()
-  await dbHelper.closeConnection()
-  return
-})
+if (process.env.NODE_ENV !== "test-ui") {
+  afterAll(async () => {
+    const { default: DbHelper } = await import("./helpers/db")
+    const dbHelper = new DbHelper()
+    await dbHelper.closeConnection()
+    return
+  })
+}
