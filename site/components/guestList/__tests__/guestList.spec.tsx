bd006ecc8393f89075e516ac38916792231f2479
kwak123 <kwak123@gmail.com>
2020-03-18 23:03:33 -0400

    extract out guestCard component

--- site/components/guestList/__tests__/guestList.spec.tsx
+++ site/components/guestList/__tests__/guestList.spec.tsx
@@ -5,12 +5,12 @@ import { render } from "@testing-library/react"
 
 describe("guestList", () => {
   it("should render guests", async () => {
-    const { findByText } = render(<GuestList guestList={mockGuests} />)
+    const { getByText } = render(<GuestList guestList={mockGuests} />)
     // TODO: 3/18/2020 See how we can query these in a promise?
     const [guestOne, guestTwo, guestThree, guestFour] = mockGuests
-    expect(await findByText(guestOne.name)).not.toBeNull()
-    expect(await findByText(guestTwo.name)).not.toBeNull()
-    expect(await findByText(guestThree.name)).not.toBeNull()
-    expect(await findByText(guestFour.name)).not.toBeNull()
+    expect(getByText(guestOne.name)).not.toBeNull()
+    expect(getByText(guestTwo.name)).not.toBeNull()
+    expect(getByText(guestThree.name)).not.toBeNull()
+    expect(getByText(guestFour.name)).not.toBeNull()
   })
 })
