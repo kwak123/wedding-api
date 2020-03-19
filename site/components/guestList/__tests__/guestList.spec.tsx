ac091206bbc91629ef31607b8927259eb96d6fe1
kwak123 <kwak123@gmail.com>
2020-03-18 22:29:05 -0400

    real tests in guestList

--- site/components/guestList/__tests__/guestList.spec.tsx
+++ site/components/guestList/__tests__/guestList.spec.tsx
@@ -4,7 +4,13 @@ import mockGuests from "../../../__mocks__/mockGuests"
 import { render } from "@testing-library/react"
 
 describe("guestList", () => {
-  it("should render guests", () => {
-    render(<GuestList guestList={mockGuests} />)
+  it("should render guests", async () => {
+    const { findByText } = render(<GuestList guestList={mockGuests} />)
+    // TODO: 3/18/2020 See how we can query these in a promise?
+    const [guestOne, guestTwo, guestThree, guestFour] = mockGuests
+    expect(await findByText(guestOne.name)).not.toBeNull()
+    expect(await findByText(guestTwo.name)).not.toBeNull()
+    expect(await findByText(guestThree.name)).not.toBeNull()
+    expect(await findByText(guestFour.name)).not.toBeNull()
   })
 })
